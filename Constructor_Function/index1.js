4e6e487ed8f50f76e4fa6559cd34bed3a8917626
Saher Fatma <saherfatma012@gmail.com>
2024-01-04 16:39:38 +0530

    my bookstore

---
+++ Constructor_Function/index1.js
@@ -0,0 +1,35 @@
+function Author(name, birthYear, nationality){
+ this.name = name;
+ this.birthYear = birthYear;
+ this.nationality = nationality;
+}
+
+function Book(title, author, genre, price){
+ this.title = title;
+ this.author = author;
+ this.genre = genre;
+ this.price = price
+}
+
+const author1 = new Author("Robert Kiyosaki", 1997, "American")
+console.log(author1);
+
+const author2 = new Author("Henary James", 1904, "American")
+console.log(author2);
+
+
+const book1 = new Book("Rich Dad Poor Dad", author1.name, "Personal Finance", 437)
+console.log(book1);
+
+const book2 = new Book("The Golden Bowl", author2.name, "Modernist literary", 789)
+console.log(book2);
+
+Book.prototype.getBookInfo = function(){
+ console.log(`Title:${this.title}`);
+ console.log(`Author:${this.author}`);
+ console.log(`Genre:${this.genre}`);
+ console.log(`Price:${this.price}`);
+}
+
+book1.getBookInfo();
+book2.getBookInfo();
